f7a1421a8ccf9b61acb7962fcdff2fa67215b9e5
rusrap3547 <t3sseract@proton.me>
2025-10-03 08:11:49 -0400

    finished first 3

---
+++ exercises/e0-example.ts
@@ -0,0 +1,11 @@
+// TypeScript version of e0-example.js
+// ...existing code...
+
+/**
+ * Exercise 0: Example
+ *
+ * Write a function `add` that takes two numbers and returns their sum.
+ * 
+ * Example:
+ *   add(2, 3) // => 5
+ */
--- exercises/e1.js
+++ exercises/e1.js
@@ -5,10 +5,8 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetNames(data) {
-  return data.planets.filter(planets => planets.name)
+	return data.planets.map((planets) => planets.name);
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-1"
